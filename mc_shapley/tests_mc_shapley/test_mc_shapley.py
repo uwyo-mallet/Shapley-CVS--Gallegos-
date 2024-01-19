6a8c2c1792214b42b040201c5df49a4676655cad
NikitaGallegos <nikitaagallegos@gmail.com>
2024-01-18 20:53:15 -0700

    adjusting unit tests to work with github actions

--- mc_shapley/tests_mc_shapley/test_mc_shapley.py
+++ mc_shapley/tests_mc_shapley/test_mc_shapley.py
@@ -76,7 +76,7 @@ def test_unit_test_5():
     unitTestTemplate(ascores=[1.02,0,0.73,0,0,0], mscores=[0.29,0,0], tmscores=[0.29, 0, 0], sscores=[0.655,0.365,0], tsscores=[0.655,0.365,0], tOrder=[["A1","A2"], ["A3"]])
 
 def test_traditional():
-    [algorithms, instances, scores] = shapley_cvs.read_file(".\\tests_mc_shapley\\test_file2.txt") 
+    [algorithms, instances, scores] = shapley_cvs.read_file("./tests_mc_shapley/test_file2.txt") 
     assert shapley_cvs.traditional_shap(algorithms, instances, scores) == test.approx(shapley_cvs.get_vbs_shap(algorithms, instances, scores), rel=1e-4)
 
 def test_readfile_1(): 
@@ -84,7 +84,7 @@ def test_readfile_1():
         instances = set(["1-1", "1-2"])
         scores = {"A11-1": 1.02, "A21-1": 0.73, "A31-1": 0.3, "A11-2": 1.0, "A21-2": 0.5, "A31-2": 0.21}
 
-        ais = shapley_cvs.read_file(".\\test_file1.txt")
+        ais = shapley_cvs.read_file("./test_file1.txt")
         assert set(ais[0]) == algorithms, "Algorithms were not read properly: " + str(ais[0]) + " does not equal " + str(algorithms)
         assert set(ais[1]) == instances, "Instances were not read in properly: " + str(ais[1]) + " does not equal " + str(instances)
         assert ais[2] == scores, 'Score dictionary is malformed: ' + str(ais[2]) + " does not equal " + str(scores)
@@ -94,14 +94,14 @@ def test_readfile_2():
         instances = set(["1-10000", "2-10000"])
         scores = {"insertion1-10000": 0, "first1-10000": 0, "random1-10000": 0.01, "insertion2-10000": 0, "first2-10000": 0.96, "random2-10000": 0.01}
 
-        ais = shapley_cvs.read_file(".\\tests_mc_shapley\\test_file2.txt")
+        ais = shapley_cvs.read_file("./tests_mc_shapley/test_file2.txt")
         assert set(ais[0]) == algorithms, "Algorithms were not read properly: " + str(ais[0]) + " does not equal " + str(algorithms) + "\n"
         assert set(ais[1]) == instances, "Instances were not read in properly: " + str(ais[1]) + " does not equal " + str(instances) + "\n"
         assert ais[2] == scores, 'Score dictionary is malformed: ' + str(ais[2]) + " does not equal " + str(scores) + "\n"
 
         t_order = {"1946": ["insertion"], "1961": ["first", "random"]}
         t_order_bysolver = {"insertion": "1946", "first": "1961", "random": "1961"}
-        temporal = shapley_cvs.read_temporal_file(".\\tests_mc_shapley\\temp_test_file2.csv", ais[0])
+        temporal = shapley_cvs.read_temporal_file("./tests_mc_shapley/temp_test_file2.csv", ais[0])
         assert temporal[0] == t_order, "Temporal order was not read properly: " + str(temporal[0]) + " does not equal " + str(t_order) + "\n" 
         assert temporal[1] == t_order_bysolver, "Temporal order by solver was not formed properly: " + str(temporal[1]) + " does not equal " + str(t_order_bysolver) + "\n" 
 
@@ -109,7 +109,7 @@ def test_readfile_3():
     algorithms = set(["A1", "A2", "A3", "A4", "A5"])
     instances = set(["1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7"])
 
-    ais = shapley_cvs.read_file(".\\tests_mc_shapley\\test_file3.csv")
+    ais = shapley_cvs.read_file("./tests_mc_shapley/test_file3.csv")
     assert set(ais[0]) == algorithms, "Algorithms were not read properly: " + str(ais[0]) + " does not equal " + str(algorithms) + "\n"
     assert set(ais[1]) == instances, "Instances were not read in properly: " + str(ais[1]) + " does not equal " + str(instances) + "\n"
     assert ais[2]["A21-3"] == 0.11, "Score dictionary is malformed: " + str(ais[2]["A21-3"]) + " does not equal 0.11" + "\n"
@@ -121,16 +121,16 @@ def test_readfile_3():
 
 def test_missing_entry(): 
     with test.raises(ValueError) as exc_info: 
-        shapley_cvs.read_file(".\\tests_mc_shapley\\test_file4.csv")
+        shapley_cvs.read_file("./tests_mc_shapley/test_file4.csv")
     assert str(exc_info.value) == "Missing Data Entry in Row: 18", "Failed to raise proper exception: " 
 
 def test_score_conflict():
     with test.raises(ValueError) as exc_info: 
-        shapley_cvs.read_file(".\\tests_mc_shapley\\test_file5.csv")
+        shapley_cvs.read_file("./tests_mc_shapley/test_file5.csv")
     assert str(exc_info.value) == "Two Different Scores for the Same Algorithm-Instance, Start at Row: 17", "Failed to raise proper exception: " 
 
 def test_temporal_missing(): 
     algorithms = ["A1", "A2", "A3", "A4", "A5"]
     with test.raises(ValueError) as exc_info: 
-        shapley_cvs.read_temporal_file(".\\tests_mc_shapley\\temp_test_file3.csv", algorithms)
+        shapley_cvs.read_temporal_file("./tests_mc_shapley/temp_test_file3.csv", algorithms)
     assert str(exc_info.value) == "No temporal information found for A5!", "Failed to raise proper exception: " 
